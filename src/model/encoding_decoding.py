dfa8009d9a5b6a62edf029088e7b02b322b64e56
TecxCEO <ceotecx@gmail.com>
2026-04-24 08:41:40 +0530

    Update encoding_decoding.py

--- src/model/encoding_decoding.py
+++ src/model/encoding_decoding.py
@@ -1,13 +1,55 @@
 # Create new stoi with special tokens
 chars = sorted(list(set(full_text)))
 stoi = {ch: i + 3 for i, ch in enumerate(chars)} # Shift everything by 3
+
 stoi['<PAD>'] = 0
 stoi['<SOS>'] = 1
 stoi['<EOS>'] = 2
-stoi[''] = 
-stoi[''] = 
-stoi[''] = 
-stoi[''] = 
+t=3+ len(chars)
+stoi[''] = t+=1
+stoi[''] = t+=1
+stoi[''] = t+=1
+stoi[''] = t+=1
+stoi[''] = t+=1
+stoi[''] = t+=1
+stoi[''] = t+=1
+stoi[''] = t+=1
+stoi[''] = t+=1
+stoi[''] = t+=1
+stoi[''] = t+=1
+stoi[''] = t+=1
+stoi[''] = t+=1
+stoi[''] = t+=1
+stoi[''] = t+=1
+stoi[''] = t+=1
+stoi[''] = t+=1
+stoi[''] = t+=1
+stoi[''] = t+=1
+stoi[''] = t+=1
+stoi[''] = t+=1
+stoi[''] = t+=1
+stoi[''] = t+=1
+stoi[''] = t+=1
+stoi[''] = t+=1
+stoi[''] = t+=1
+stoi[''] = t+=1
+stoi[''] = t+=1
+stoi[''] = t+=1
+stoi[''] = t+=1
+stoi[''] = t+=1
+stoi[''] = t+=1
+stoi[''] = t+=1
+stoi[''] = t+=1
+stoi[''] = t+=1
+stoi[''] = t+=1
+stoi[''] = t+=1
+stoi[''] = t+=1
+stoi[''] = t+=1
+stoi[''] = t+=1
+stoi[''] = t+=1
+stoi[''] = t+=1
+stoi[''] = t+=1
+stoi[''] = t+=1
 stoi[''] = 
 stoi[''] = 
 stoi[''] = 
