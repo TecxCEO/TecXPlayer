a8b29b807e03252d95716f74420e92f4845e4a13
TecxCEO <ceotecx@gmail.com>
2026-04-24 09:40:55 +0530

    Update encoding_decoding.py

--- src/model/encoding_decoding.py
+++ src/model/encoding_decoding.py
@@ -1,12 +1,15 @@
 # Create new stoi with special tokens
 chars = sorted(list(set(full_text)))
-stoi = {ch: i + 3 for i, ch in enumerate(chars)} # Shift everything by 3
-
 stoi['<PAD>'] = 0
 stoi['<SOS>'] = 1
 stoi['<EOS>'] = 2
-# t=3+ len(chars)
 t= len(stoi)-1
+#stoi = {ch: i + 3 for i, ch in enumerate(chars)} # Shift everything by 3
+stoi = {ch: (t+=1) for i, ch in enumerate(chars)} # Shift everything by 3
+
+
+# t=3+ len(chars)
+# t= len(stoi)-1
 
 """
 stoi['<>'] = t+=1
