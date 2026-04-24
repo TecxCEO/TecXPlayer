817d4ffde954170a57cca06520920f203469b2d2
TecxCEO <ceotecx@gmail.com>
2026-04-24 09:45:17 +0530

    Update encoding_decoding.py

--- src/model/encoding_decoding.py
+++ src/model/encoding_decoding.py
@@ -137,23 +137,23 @@ stoi[''] =
 """
 
 # itos = {i: ch for ch, i in stoi.items()} # Reverse map
-def get_nested_value(self, full_text):
+def createTokens(self, full_text):
   t=len(stoi)-1
     if isinstance(full_text, dict):
       for key, value in (full_text):
         str="'<"+key+">'"
         stoi[str] = t+=1
         if isinstance(value, dict):
-          self.get_nested_value(value)
-        elif isinstance(value, (dict, list)):
+          self.createTokens(value)
+        elif not isinstance(value, (dict, list)):
           stoi[value] = t+=1
       return stoi
-def createTokens(self,full_text):
+#def createTokens(self,full_text):
   #chars = sorted(list(set(full_text)))
   #stoi = {ch: i + 3 for i, ch in enumerate(chars)} # Shift everything by 3
   #stoi['<PAD>'] = 0
   #stoi['<SOS>'] = 1
   #stoi['<EOS>'] = 2
   #t=3+ len(chars)
-  get_nested_value(self, full_text)
+  # self.get_nested_value(self, full_text)
       
