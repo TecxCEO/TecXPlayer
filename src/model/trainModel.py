a823344de9072905f0d88c831960d1c675983b3b
TecxCEO <ceotecx@gmail.com>
2026-04-26 12:29:49 +0530

    Update trainModel.py

--- src/model/trainModel.py
+++ src/model/trainModel.py
@@ -11,25 +11,27 @@ from encoding_decoding import EncodeDecode as ed
 if __name__ == "__main__":
     # Setup Example
     vocab_size = 80  # Size of your 'stoi' map
+    max_epoch = 11
     model = tm.DictionaryTransformer(vocab_size=vocab_size, d_model=128, nhead=8, num_layers=4, num_classes=3)
     # model = DictionaryTransformer(vocab_size=vocab_size, d_model=128, nhead=20, num_layers=12, num_classes=3)
     input = imd.ImportDataset() #
     model(input) #
 best_val_loss = float('inf') # Start with infinity
 ##
-# Inside your epoch loop, after calculating avg_val_loss:
-if avg_val_loss < best_val_loss:
-  best_val_loss = avg_val_loss
-  # Save the model state
-  checkpoint = {
+checkpoint = {
     'epoch': epoch + 1,
     'model_state_dict': model.state_dict(),
     'optimizer_state_dict': optimizer.state_dict(),
     'best_val_loss': best_val_loss,
     'stoi': ed.createTokens() # Saving the vocabulary is critical!
     #'stoi': stoi # Saving the vocabulary is critical!
-  }
-  torch.save(checkpoint, 'best_dictionary_model.pth')
-  print(f"--> Saved new best model with Val Loss: {best_val_loss:.4f}")
+}
+for epoch in range(max_epoch):
+    # Inside your epoch loop, after calculating avg_val_loss:
+    if avg_val_loss < best_val_loss:
+        best_val_loss = avg_val_loss
+        # Save the model state
+        torch.save(checkpoint, 'best_dictionary_model.pth')
+        print(f"--> Saved new best model with Val Loss: {best_val_loss:.4f}")
 # Save progress
 torch.save(model.state_dict(), f"checkpoint_epoch_{epoch}.pth")
