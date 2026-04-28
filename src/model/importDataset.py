0349b2d8ce27a965b3b87f8a3ca55ae20aba37a5
TecxCEO <ceotecx@gmail.com>
2026-04-28 09:17:44 +0530

    Update importDataset.py

--- src/model/importDataset.py
+++ src/model/importDataset.py
@@ -92,7 +92,7 @@ if __name__ == "__main__":
     st_mv_data = []
     st_mv_data += idc.createInputString(idc.data["solution"])
     print(f"st_mv_data = {st_mv_data}")
-    l=0
+    l+=1
     for smd in st_mv_data:
         st_mv_data_list = idc.convertStateToList(smd)
         print(f"st_mv_data_list {l++} = {st_mv_data_list}\n\n")
