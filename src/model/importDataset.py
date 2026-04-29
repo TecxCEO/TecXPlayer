c898583cc096681fe92cc59f62a50042281b9a44
TecxCEO <ceotecx@gmail.com>
2026-04-29 21:28:37 +0530

    Update importDataset.py

--- src/model/importDataset.py
+++ src/model/importDataset.py
@@ -119,8 +119,8 @@ if __name__ == "__main__":
     print(f" st_data = {next(iterator, None)}")
     idc.get_nested_value(idc.data["solution"])
     # This will trigger every print inside the function as it loops
-    for result in idc.get_nested_value(idc.data["solution"]):
-        print(f"Got result: {result}")
+    # for result in idc.get_nested_value(idc.data["solution"]):
+        # print(f"Got result: {result}")
     st_mv_data = []
     st_mv_data += idc.createInputString(idc.data["solution"])
     ###print(f"st_mv_data = {st_mv_data}")
