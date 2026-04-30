c87775b2e837ce3a4b5586b104c662820d4882f4
TecxCEO <ceotecx@gmail.com>
2026-04-30 08:49:23 +0530

    Update encoding_decoding.py

--- src/model/encoding_decoding.py
+++ src/model/encoding_decoding.py
@@ -66,7 +66,7 @@ class EncodeDecode:
       for key, value in full_text.items():
         #if not self.stoi.get(key) and isinstance(value, dict):
         # if not self.stoi.get(key) and key != "state" and isinstance(value, dict):  
-        str="'<"+key+">'"
+        str="<"+key+">"
         #if not self.stoi.get(key) and isinstance(value, dict):
         if not self.stoi.get(str) and isinstance(value, dict):
           t+=1
