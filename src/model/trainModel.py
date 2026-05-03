6f38bfd5fbf8959d242e94c128baf3436021ab77
TecxCEO <ceotecx@gmail.com>
2026-05-03 10:14:44 +0530

    Update trainModel.py

--- src/model/trainModel.py
+++ src/model/trainModel.py
@@ -181,8 +181,8 @@ if __name__ == "__main__":
         # Change this:
         # stmdl_enc += edc.stoi(stmdl_in)
         # To this:
-        stmdl_enc.append(edc.stoi[stmdl_in])
-        #stmdl_enc.append(edc.encode(stmdl_in))
+        ###### stmdl_enc.append(edc.stoi[stmdl_in])
+        stmdl_enc.append(edc.encode(stmdl_in))
         # import torch
         # Convert your list to a tensor
         stmdl_tensor += torch.tensor(stmdl_enc)
