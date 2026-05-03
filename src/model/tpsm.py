392156fc2a391cdb8367bf3f27b5c42c8427aaba
TecxCEO <ceotecx@gmail.com>
2026-05-03 13:01:53 +0530

    Update tpsm.py

--- src/model/tpsm.py
+++ src/model/tpsm.py
@@ -6,6 +6,19 @@ import torch.nn as nn
 import math
 from torch.nn import functional as F
 
+batch_size = 64 # how many independent sequences will we process in parallel?
+block_size = 256 # what is the maximum context length for predictions?
+max_iters = 5000
+eval_interval = 500
+learning_rate = 3e-4
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+eval_iters = 200
+n_embd = 384
+n_head = 6
+n_layer = 6
+dropout = 0.2
+# ------------
+
 class DictionaryTransformer(nn.Module):
     def __init__(self, vocab_size, d_model, nhead, num_layers, num_classes):
         super(DictionaryTransformer, self).__init__()
