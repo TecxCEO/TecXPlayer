91f5b2246108b6a18a33939095a860aa01e47e50
TecxCEO <ceotecx@gmail.com>
2026-05-03 17:55:55 +0530

    Update inferencing.py

--- src/model/inferencing.py
+++ src/model/inferencing.py
@@ -13,7 +13,7 @@ import torch
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 edc = ed.EncodeDecode()
-model_path = 'models/best_dictionary_model.pth'
+model_path = 'models/best_dictionary_model1.pth'
 # model_path ="models/checkpoint2_epoch_10.pth"
 checkpoint = torch.load(model_path)
 print(checkpoint)
