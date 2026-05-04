c5a3d02dbd303c76e3f6a57faf0045aaf4705e3e
TecxCEO <ceotecx@gmail.com>
2026-05-04 08:45:06 +0530

    Update trainModel.py

--- src/model/trainModel.py
+++ src/model/trainModel.py
@@ -208,14 +208,14 @@ if __name__ == "__main__":
         for i in range(len(stmdl) // 3):
             print(f" Epoch no = {epoch}\n, Loop no = {i}\n")
             stmdlin  = []
-            stmdlin  = ['<sos>']
+            stmdlin  = ['<SOS>']
             stmdlin += stmdl[3*i]
             ####stmdlin += [stmdl[3*i]]
             # stmdlin += stmdl[3*i]
             stmdlin += ['<'+stmdl[3*i+1]+'>']
             stmdlin += stmdl[3*i+2]
             ########stmdlin += [stmdl[3*i+2]]
-            stmdlin  = ['<eos>']
+            stmdlin  = ['<EOS>']
             # stmdlin += stmdl[3*i+2]
             # Prepend '<sos>' and append '<eos>' to every sentence
             ##tagged_data = ['<sos> ' + x + ' <eos>' for x in stmdlin]
