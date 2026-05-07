b68502141b38b1f6e4cb1455492421b54d2a3c34
TecxCEO <ceotecx@gmail.com>
2026-05-07 17:15:30 +0530

    Update trainModel.py

--- src/model/trainModel.py
+++ src/model/trainModel.py
@@ -26,7 +26,7 @@ def get_nested_data(data, edc, idc):
     for key, value in data.items():
         #####
         st_mv_data = []
-        st_mv_data += idc.createInputString(value)
+        st_mv_data += idc.createInputString(value) if value['state'] and len(value) == (16,19) else None
         ####
         ######$#$st_mv_data += idc.createInputString(data["solution"])
         ####
