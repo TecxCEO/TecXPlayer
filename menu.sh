a76cb6ebcbabafa1e2dad75b639d7df6d8269cdb
TecxCEO <ceotecx@gmail.com>
2026-05-11 08:12:37 +0530

    Create menu.sh

---
+++ menu.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Define the visual prompt for the menu
+PS3="Please enter your choice (1-7): "
+
+# List the menu options
+options=("Show System Info" "Update Packages" "Print Hello World" "Exit")
+
+# Start the interactive loop
+select opt in "${options[@]}"
+do
+    case $opt in
+        "Show System Info")
+            echo "--- System Info ---"
+            uname -a
+            echo "--------------------"
+            ;;
+        "Update Packages")
+            echo "Updating Termux packages..."
+            pkg update && pkg upgrade -y
+            ;;
+        "Print Hello World")
+            echo "===================="
+            echo "Hello, World!"
+            echo "===================="
+            ;;
+        "Exit")
+            echo "Goodbye!"
+            break
+            ;;
+        *) 
+            echo "Invalid option $REPLY. Please choose a valid number."
+            ;;
+    esac
+done
+
+cd TecXPlayer
