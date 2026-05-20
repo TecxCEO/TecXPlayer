d018719d44a8990988d1109aaacc17bb25fdf100
TecxCEO <ceotecx@gmail.com>
2026-05-20 08:15:48 +0530

    Update trainTecXModel.py

--- src/model/trainTecXModel.py
+++ src/model/trainTecXModel.py
@@ -124,8 +124,9 @@ def createTVData(file, edctv = None, idctv = None):
         stmdlenc += edc.encode(stmdlin)
     return stmdlenc, edc, idc
 def file_dir_nested(directory = "./data/dataset", d_list = []):
-    dir = directory
-    folder_list = [d for d in os.listdir(dir) if os.path.isdir(os.path.join(dir,d))]
+    #dir = directory
+    folder_list = directory ####
+    folder_list = [d for d in os.listdir(directory) if os.path.isdir(os.path.join(directory,d))]
     #for folder_name in folder_list:
     # directories = [d for d in os.listdir(path_given) if os.path.isdir(os.path.join(path_given,d))]
     #dir = "./data/dataset"
