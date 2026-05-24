adc04ebd444a616112d18545f3a9e296e1b0909a
TecxCEO <ceotecx@gmail.com>
2026-05-24 17:27:02 +0530

    Update inferencing.py

--- src/model/inferencing.py
+++ src/model/inferencing.py
@@ -109,7 +109,8 @@ print(f"Keys in state_given_to_solve = {state_given_to_solve.keys()}\n")
 print(f"Value in state_given_to_solve = {state_given_to_solve.values()}\n")
 stk = state_given_to_solve.keys()
 stv = state_given_to_solve.values()
-p = ["<SOS>"]
+p = []
+#####p = ["<SOS>"]
 p.extend(stk)
 p += ["<rgy>"]
 #p.extend(stv)
