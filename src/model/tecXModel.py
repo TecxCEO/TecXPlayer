3b8acacb9e4078588364a97f0346ec18f8b5f67d
TecxCEO <ceotecx@gmail.com>
2026-05-29 10:03:30 +0530

    Update tecXModel.py

--- src/model/tecXModel.py
+++ src/model/tecXModel.py
@@ -14,8 +14,8 @@ learning_rate = 3e-4
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 eval_iters = 200
 n_embd = 384
-n_head = 20 #6
-n_layer = 20 # 6
+n_head = 8 #20 #6
+n_layer = 6 #20 # 6
 dropout = 0.2
 # ------------
 torch.manual_seed(1337)
