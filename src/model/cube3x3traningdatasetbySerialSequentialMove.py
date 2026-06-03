a12cc50c5b766917e2920d7232ae5edc5d2ae7d3
TecxCEO <ceotecx@gmail.com>
2026-06-03 15:18:42 +0530

    Update cube3x3traningdatasetbySerialSequentialMove.py

--- src/model/cube3x3traningdatasetbySerialSequentialMove.py
+++ src/model/cube3x3traningdatasetbySerialSequentialMove.py
@@ -14,7 +14,8 @@ class Solver(c3x3):
       "puzzle": {
         "puzzle_given": self.current_state,
         "puzzle_status":False,
-        "moves_to_solve_puzzle":""
+        "moves_to_solve_puzzle":"",
+        "moves_history":""
       },
       "solution":self.current_state
     }
@@ -29,7 +30,7 @@ class Solver(c3x3):
         my_data = json.load(rf)
       # 2. Update a key (no matter how deep it is)      
       if my_data["puzzle"]["puzzle_status"]==False:
-        self.update_nested_key(my_data["solution"],my_data["puzzle"]["puzzle_status"],my_data["puzzle"]["moves_to_solve_puzzle"])
+        self.update_nested_key(my_data["solution"],my_data["puzzle"]["puzzle_status"],my_data["puzzle"]["moves_to_solve_puzzle"],my_data["puzzle"]["moves_history"])
         with open(self.filename, "w") as wf:
           json.dump(my_data, wf)
           #json.dump(my_data, wf, indent=4)
@@ -63,10 +64,16 @@ class Solver(c3x3):
           #if len(states) in [15,18] and len(move_list) in [15,18] and status is False:
           if len(states) in [1,15,18] and len(move_list) in [1,15,18]:
             #print(f"data length= {len(data)}")
+            mh = []
             for i in range(len(states)):
               data.update({move_list[i]:states[i]})
-              ####moved_history.update({move_list[i]:""}) ####
+              mh += move_list[i]
+            if len(moves_history) == 15:
+              moves_history += [mh]
           return data, moves_history, status, moved_history
+      if len(moves_history) == 16 and len(moves_history[15]) in [18, 15]:
+        del data[moves_history[0]][moves_history[1]][moves_history[2]][moves_history[3]][moves_history[4]][moves_history[5]][moves_history[6]][moves_history[7]][moves_history[8]][moves_history[9]][moves_history[10]][moves_history[11]][moves_history[12]][moves_history[13]][moves_history[14]][moves_history[15]]
+      
       # if (len(data)==16 or len(data)==19 or len(data)==20 )and len(moves_history) <16:
       if len(data) < 20 and len(moves_history) < 16:
         for key, value in data.items():
