54bb3c200f8b18aec464d561c0eb5d4ccbcd1c27
TecxCEO <ceotecx@gmail.com>
2026-06-03 14:46:57 +0530

    Update cube3x3traningdatasetbySerialSequentialMove.py

--- src/model/cube3x3traningdatasetbySerialSequentialMove.py
+++ src/model/cube3x3traningdatasetbySerialSequentialMove.py
@@ -67,7 +67,8 @@ class Solver(c3x3):
               data.update({move_list[i]:states[i]})
               ####moved_history.update({move_list[i]:""}) ####
           return data, moves_history, status, moved_history
-      if (len(data)==16 or len(data)==19 or len(data)==20 )and len(moves_history) <16
+      # if (len(data)==16 or len(data)==19 or len(data)==20 )and len(moves_history) <16:
+      if len(data) < 20 and len(moves_history) < 16:
         for key, value in data.items():
           if key!="state" and (len(value) in [16,19,20] or len(data[key]) in [15,18,20]):
             if (moves_history and moves_history[-1]!=key) or not moves_history:
@@ -76,9 +77,10 @@ class Solver(c3x3):
               if moved_history is None:
                 moved_history.update({key:""})
               self.update_nested_key(value,status,mtsp,moves_history+[key], moved_history[key])
-            if status == True and mtsp:
-              print(f"mtsp={mtsp}")
-              return
+              return ####
+            #if status == True and mtsp:
+              #print(f"mtsp={mtsp}")
+              #return
         #print(f"rec loop end.")
         return
 if __name__=="__main__":
