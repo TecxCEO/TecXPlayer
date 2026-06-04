38bf2718cfd3e227975a0e486ee1c4fad3bdf8c9
TecxCEO <ceotecx@gmail.com>
2026-06-04 09:11:28 +0530

    Update cube3x3traningdatasetbySerialSequentialMove.py

--- src/model/cube3x3traningdatasetbySerialSequentialMove.py
+++ src/model/cube3x3traningdatasetbySerialSequentialMove.py
@@ -118,10 +118,10 @@ class Solver(c3x3):
               # self.update_nested_key(value,status,mtsp,moves_history+[key], moved_history[key])
               print(f"data = {data}")
               moves_history += [key]
-              print(f"moves_history = {moves_history}")
+              print(f"moves_history before calling = {moves_history}")
               
               # self.update_nested_key(value,status,mtsp,moves_history+[key], data_batch[key])
-              self.update_nested_key(value,status,mtsp,moves_history+[key], data_batch[key])
+              self.update_nested_key(value,status,mtsp,moves_history, data_batch[key])
               print(f"data after nested calling = {data}")
               print(f"moves_history after nested calling= {moves_history}")
               return ####
