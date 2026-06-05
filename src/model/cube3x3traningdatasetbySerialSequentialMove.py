d4c3111da67b77a8476c37a51455cb6910c2f818
TecxCEO <ceotecx@gmail.com>
2026-06-05 20:44:39 +0530

    Update cube3x3traningdatasetbySerialSequentialMove.py

--- src/model/cube3x3traningdatasetbySerialSequentialMove.py
+++ src/model/cube3x3traningdatasetbySerialSequentialMove.py
@@ -30,10 +30,10 @@ class Solver(c3x3):
     print(f"while loop is going to started.")
     while True:
       ####print(f"while loop no = {while_loop }  are started.")
-      print(f"moves_history before start = {my_data["puzzle"]["moves_history"]}")
       # 1. Load your file
       with open(self.filename, "r") as rf:
         my_data = json.load(rf)
+      print(f"moves_history before start = {my_data["puzzle"]["moves_history"]}")
       if len(my_data["puzzle"]["moves_history"]) == 16:
         #### del data_batch
         print(f"data_batch before start = {data_batch}")
