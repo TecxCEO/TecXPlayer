9cf885e2ecbc7c6d6f2a3e73061d2edcfb245ff6
TecxCEO <ceotecx@gmail.com>
2026-06-06 08:34:48 +0530

    Update cube3x3traningdatasetbySerialSequentialMove.py

--- src/model/cube3x3traningdatasetbySerialSequentialMove.py
+++ src/model/cube3x3traningdatasetbySerialSequentialMove.py
@@ -129,13 +129,13 @@ class Solver(c3x3):
                 data_batch = {}
         data_batch.update({"state": data["state"]})
         #items_list = list(data.items())
-        items_list = list(data.copy().items())
+        ########items_list = list(data.copy().items())
         #items_list = list(data.copy.items())
         #items_list = [item for item in data.items()]
         #for key, value in data.items():
         for key, value in items_list:
           if key!="state" and (len(value) in [16,19,20] or len(data[key]) in [15,18,20]):
-            if (moves_history and moves_history[-1]!=key) or not moves_history:
+            #if (moves_history and moves_history[-1]!=key) or not moves_history:
               print(f" key = {key}")
               # if moved_history[key] and moved_history[key] is not in [None, ""]:
               # el
