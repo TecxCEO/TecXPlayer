c395f3234ceb6c7d133c23b1c1ef7f63271d713c
TecxCEO <ceotecx@gmail.com>
2026-06-06 08:15:44 +0530

    Update cube3x3traningdatasetbySerialSequentialMove.py

--- src/model/cube3x3traningdatasetbySerialSequentialMove.py
+++ src/model/cube3x3traningdatasetbySerialSequentialMove.py
@@ -129,7 +129,7 @@ class Solver(c3x3):
                 data_batch = {}
         data_batch.update({"state": data["state"]})
         #items_list = list(data.items())
-        items_list = list((data.items()).copy)
+        items_list = list(data.items()).copy
         #for key, value in data.items():
         for key, value in items_list:
           if key!="state" and (len(value) in [16,19,20] or len(data[key]) in [15,18,20]):
