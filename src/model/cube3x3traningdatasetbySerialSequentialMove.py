5d8c4f59b0ffb77a29825142af4dfcb0ea599b71
TecxCEO <ceotecx@gmail.com>
2026-06-06 19:34:09 +0530

    Update cube3x3traningdatasetbySerialSequentialMove.py

--- src/model/cube3x3traningdatasetbySerialSequentialMove.py
+++ src/model/cube3x3traningdatasetbySerialSequentialMove.py
@@ -123,7 +123,8 @@ class Solver(c3x3):
           return data, moves_history, status, data_batch
       # if len(moves_history) == 16 and len(moves_history[15]) in [18, 15]:
       print(f" moves_history ={moves_history}")
-      if len(moves_history) == 16 and len(moves_history[15]) in [18, 15]:
+      #if len(moves_history) == 16 and len(moves_history[15]) in [18, 15]:
+      if len(moves_history) == 16 and len(str(moves_history[15])) in [18, 15]:
         print(f" moves_history at length = {len(moves_history)}")
         print(f" moves_history = {moves_history}")
         print(f" moves_history[15] at length = {len(moves_history[15])}")
@@ -175,8 +176,8 @@ class Solver(c3x3):
               print(f"moves_history length = {len(moves_history)}")
               # self.update_nested_key(value,status,mtsp,moves_history+[key], data_batch[key])
               #if key == removed_key:
-              #if key == moves_history[0]:
-              if moves_history[0] in (key, 16):
+              if key == moves_history[0]:
+                # if moves_history[0] in (key, 16):
                 self.update_nested_key(value,status,mtsp,moves_history, data_batch[key])
               #if len(data.items())>len(moves_history):
               #moves_history.insert(0, key) if locals().get("removed_key") and removed_key == key else None
