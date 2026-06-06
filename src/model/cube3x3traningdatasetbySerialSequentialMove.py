0aa7e8fedc81b06a196eb2d94c8f522787cc087b
TecxCEO <ceotecx@gmail.com>
2026-06-06 16:15:36 +0530

    Update cube3x3traningdatasetbySerialSequentialMove.py

--- src/model/cube3x3traningdatasetbySerialSequentialMove.py
+++ src/model/cube3x3traningdatasetbySerialSequentialMove.py
@@ -160,7 +160,8 @@ class Solver(c3x3):
               #moves_history.insert(0, key) if locals().get("removed_key") and removed_key == key else None
               print(f" After function return, key = { key }")
               ###print(f" Removed_key = { removed_key }")
-              if locals().get("removed_key") and removed_key == key:
+              #if locals().get("removed_key") and removed_key == key:
+              if locals().get("removed_key") :
                 moves_history.insert(0, key)
                 print(f"moves_history after calling the nested function = {moves_history}")
                 print(f"moves_history length = {len(moves_history)}")
