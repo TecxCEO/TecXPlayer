f60591bb573639410c83d83979f2ee8028846c05
TecxCEO <ceotecx@gmail.com>
2026-06-06 13:52:25 +0530

    Update cube3x3traningdatasetbySerialSequentialMove.py

--- src/model/cube3x3traningdatasetbySerialSequentialMove.py
+++ src/model/cube3x3traningdatasetbySerialSequentialMove.py
@@ -144,7 +144,7 @@ class Solver(c3x3):
               data_batch.update({key:""})
               # self.update_nested_key(value,status,mtsp,moves_history+[key], moved_history[key])
               print(f"data_batch = {data_batch}")
-              if (moves_history and moves_history[-1]!=key) or 
+              # if (moves_history and moves_history[-1]!=key) or not moves_history:
               if not moves_history:
                 moves_history += [key]
               if moves_history and len(moves_history) >= 1:
