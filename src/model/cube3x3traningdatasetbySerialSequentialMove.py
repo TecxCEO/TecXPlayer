e51b19725a8e139ce51cd2fd4b912a9af70a0229
TecxCEO <ceotecx@gmail.com>
2026-06-07 17:58:53 +0530

    Update cube3x3traningdatasetbySerialSequentialMove.py

--- src/model/cube3x3traningdatasetbySerialSequentialMove.py
+++ src/model/cube3x3traningdatasetbySerialSequentialMove.py
@@ -59,6 +59,7 @@ class Solver(c3x3):
     print(f" data_to_process at length = {len(data_to_process)}")
     print(f" data_to_process keys = {data_to_process.keys()}")
     print(f" index = {index}")
+    """
     #print(f" data_to_process = {data_to_process}")
     if len(data_to_process) <=2 and (data_to_process.keys() in [[moves_history[index]], "state" ]):
       print(f"In The delete_and_clean function's if statement")
@@ -68,7 +69,9 @@ class Solver(c3x3):
       print(f" data_to_process keys = {data_to_process.keys()}")
       del data_to_process[moves_history[index]]
       return 
-    elif len(data_to_process) > 2 and  index < len(moves_history)-1:
+    """
+    ####elif len(data_to_process) > 2 and  index < len(moves_history)-1:
+    if len(data_to_process) >= 2 and  index < len(moves_history)-1:
       print(f" In The delete_and_clean function's elif statement")
       print(f" data_to_process at length = {len(data_to_process)}")
       #print(f" data_to_process = {data_to_process}")
@@ -80,6 +83,9 @@ class Solver(c3x3):
         print(f" index = {index}")
         #print(f" data_to_process = {data_to_process}")
         self.delete_and_clean(data_to_process[moves_history[index]], moves_history, index+1)
+        if data_to_process[moves_history[index]] and len(data_to_process[moves_history[index]]) == 1 and data_to_process[moves_history[index]][0] == "state":
+          del data_to_process[moves_history[index]]
+          del moves_history[index]
       elif index == len(moves_history)-2 and len(data_to_process[moves_history[index]]) in [16, 15] :
         print(f"  In The delete_and_clean function's elif's elif. ")
         print(f" data_to_process at length = {len(data_to_process)}")
