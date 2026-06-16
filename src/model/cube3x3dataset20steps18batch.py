91048436e2670f082f52a769b49e750e4b9178a9
TecxCEO <ceotecx@gmail.com>
2026-06-16 14:35:42 +0530

    Update cube3x3dataset20steps18batch.py

--- src/model/cube3x3dataset20steps18batch.py
+++ src/model/cube3x3dataset20steps18batch.py
@@ -94,7 +94,7 @@ class Solver(c3x3):
           p_moves_history += []
           #p_moves_history[pk] = []
           # print(f"p_moves_history[{pk}] = {p_moves_history[pk]} ")
-        if p_moves_history and locals().get(p_moves_history[pk]):
+        elif p_moves_history and locals().get(p_moves_history[pk]):
           moves_history = p_moves_history[pk]
           print(f"DEBUG: pk value is {pk}, list total length is {len(p_moves_history)}")
           print(f"p_moves_history[{pk}] = {p_moves_history[pk]}")
