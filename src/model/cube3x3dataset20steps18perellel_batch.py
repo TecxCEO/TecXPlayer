e6bafbbf7e281f305edb37ed060d6a231e4c9d6c
TecxCEO <ceotecx@gmail.com>
2026-06-20 09:31:02 +0530

    Update cube3x3dataset20steps18perellel_batch.py

--- src/model/cube3x3dataset20steps18perellel_batch.py
+++ src/model/cube3x3dataset20steps18perellel_batch.py
@@ -193,7 +193,7 @@ class Solver(c3x3):
               self.update_nested_key(value,status,mtsp,moves_history,data_batch[key])
               if locals().get("removed_key") :
                 moves_history.insert(0, removed_key)
-            elif moves_history and ( len(moves_history) ==1 and key != moves_history[0]) :
+            elif moves_history and ( len(moves_history) >=1 and key != moves_history[0]) :
               print(f" going back to shift next key.")
               print(f" moves_history at back shift  = {moves_history} ")
               if locals().get("removed_key") :
