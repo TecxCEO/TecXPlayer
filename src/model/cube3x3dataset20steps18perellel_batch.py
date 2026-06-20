9aebcd5f247aeb022e96aae720033443d8abb0fc
TecxCEO <ceotecx@gmail.com>
2026-06-20 09:41:37 +0530

    Update cube3x3dataset20steps18perellel_batch.py

--- src/model/cube3x3dataset20steps18perellel_batch.py
+++ src/model/cube3x3dataset20steps18perellel_batch.py
@@ -51,7 +51,7 @@ class Solver(c3x3):
         print(f" pkm in while loop = {self.pkm} ")
         print(f" my_data[puzzle][moves_history] in while loop = {my_data["puzzle"]["moves_history"]} and len = {len(my_data["puzzle"]["moves_history"])} ")
         print(f" my_data[puzzle][p_moves_history] in while loop = {my_data["puzzle"]["p_moves_history"]} ")
-        time.sleep(7)
+        time.sleep(4)
         #print(f" my data = { my_data}")
         with open(self.filename, "w") as wf:
           #json.dump(my_data, wf)
