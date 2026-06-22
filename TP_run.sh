316cd57c0f154f7b815e266de96982d69b2e68cf
TecxCEO <ceotecx@gmail.com>
2026-06-22 20:32:01 +0530

    Update TP_run.sh

--- TP_run.sh
+++ TP_run.sh
@@ -1,19 +1,7 @@
 chmod +x TecXPlayer/run.sh
-TecXPlayer/run.sh
+./TecXPlayer/run.sh
 pause
 cd TecXPlayer
-chmod +x run.sh
+./chmod +x run.sh
 run.sh
-
 pause
-
-
-cd ~
-chmod +x TecXPlayer/run.sh
-cd TecXPlayer
-TecXPlayer/run.sh
-rm a.json
-git pull
-clear
-python src/model/trainModel.py
-run.sh
