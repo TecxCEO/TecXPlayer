5cb94e183135d6b2fa2f8c56126dc76e9606e2c5
TecxCEO <ceotecx@gmail.com>
2026-06-25 19:18:51 +0530

    Update createsamemovelist.py

--- src/model/createsamemovelist.py
+++ src/model/createsamemovelist.py
@@ -1,14 +1,17 @@
 move_paths=["<rgy>","<rgw>","<rgo>","<rby>","<rbw>","<rbo>","<grw>","<gry>","<grb>","<gow>","<goy>","<gob>","<yrg>","<yrb>","<yrw>","<yog>","<yob>","<yow>"]
 mosf={'b':'g','g':'b','o':'r','r':'o','w':'y','y':'w'}
 # 1. Three-Character Family Tokens (8 Families * 6 Flips = 48 Tokens)
-for fc in move_paths.keys():
-  for sc in mosf.keys():
-    for tc in mosf.keys():
-      if sc not in [fc, mosf[fc]] and tc not in [fc, mosf[fc], sc, mosf[sc]]:
-        f=moving_step.strip()[0]
-        s=moving_step.strip()[1]
-        cc=moving_step.strip()[2]
-        
+for moving_step in move_paths.keys():
+  f=moving_step.strip()[1]
+  s=moving_step.strip()[2]
+  t=moving_step.strip()[3]
+  if locals.get(same_move):
+    same_move += f"<{f}{s}{t}>"
+    same_move += f"<{mosf[]}{}{}>"
+  elif locals.get(same_move) is not exist:
+    same_move = f"<{f}{s}{t}>"
+    
+  
         self._add_token(f"{fc}{sc}{tc}", "Tier_5_Cube", "3_Char_Family_Token")
         self._add_token(f"{fc}{tc}{sc}", "Tier_5_Cube", "3_Char_Family_Token")
         self._add_token(f"{sc}{fc}{tc}", "Tier_5_Cube", "3_Char_Family_Token")
