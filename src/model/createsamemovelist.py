b412900d2a7dbc9071d3b7abb032d8426c9dd480
TecxCEO <ceotecx@gmail.com>
2026-06-25 20:29:22 +0530

    Update createsamemovelist.py

--- src/model/createsamemovelist.py
+++ src/model/createsamemovelist.py
@@ -1,17 +1,24 @@
 move_paths=["<rgy>","<rgw>","<rgo>","<rby>","<rbw>","<rbo>","<grw>","<gry>","<grb>","<gow>","<goy>","<gob>","<yrg>","<yrb>","<yrw>","<yog>","<yob>","<yow>"]
 mosf={'b':'g','g':'b','o':'r','r':'o','w':'y','y':'w'}
 # 1. Three-Character Family Tokens (8 Families * 6 Flips = 48 Tokens)
+same_move_list = {}
 for moving_step in move_paths.keys():
+  same_move = None
   f=moving_step.strip()[1]
   s=moving_step.strip()[2]
   t=moving_step.strip()[3]
+  if locals.get(same_move) is not exist or None:
+    same_move = [f"<{f}{s}{t}>"]
   if locals.get(same_move):
     #same_move += f"<{f}{s}{t}>"
     same_move += f"<{t}{s}{mosf[f]}>"
     same_move += f"<{mosf[f]}{s}{mosf[t]}>"
     same_move += f"<{mosf[t]}{s}{f}>"
-  elif locals.get(same_move) is not exist:
-    same_move = f"<{f}{s}{t}>"
+  same_move_list.update(same_move[0]: same_move)
+print(f" same move = {same_move}")
+file= "list_of_same_move.json"
+with open(file, 'r') as f:
+  data = json.dump(f)
     
     # self._add_token(f"{fc}{sc}{tc}", "Tier_5_Cube", "3_Char_Family_Token")
     # self._add_token(f"{fc}{tc}{sc}", "Tier_5_Cube", "3_Char_Family_Token")
