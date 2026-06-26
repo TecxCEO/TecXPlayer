ec56d435d459026493c2141a2088d7ed3f8a9c10
TecxCEO <ceotecx@gmail.com>
2026-06-26 06:32:19 +0530

    Update createsamemovelist.py

--- src/model/createsamemovelist.py
+++ src/model/createsamemovelist.py
@@ -3,19 +3,30 @@ move_paths=["<rgy>","<rgw>","<rgo>","<rby>","<rbw>","<rbo>","<grw>","<gry>","<gr
 mosf={'b':'g','g':'b','o':'r','r':'o','w':'y','y':'w'}
 # 1. Three-Character Family Tokens (8 Families * 6 Flips = 48 Tokens)
 same_move_list = {}
+a,b, c = None
 for moving_step in move_paths:
   same_move = None #[]
   f=moving_step.strip()[1]
   s=moving_step.strip()[2]
   t=moving_step.strip()[3]
-  #if get(same_move) is not exist or None:
-  same_move = [f"<{f}{s}{t}>"]
-  #if locals.get(same_move):
-  #same_move += f"<{f}{s}{t}>"
-  same_move += [f"<{t}{s}{mosf[f]}>"]
-  same_move += [f"<{mosf[f]}{s}{mosf[t]}>"]
-  same_move += [f"<{mosf[t]}{s}{f}>"]
-  same_move_list.update({same_move[0]: same_move})
+  if f != mosf[t]:
+    if b is None or b is not in (t, mosf[t]):
+      a = f
+      b = t
+    if b == mosf[t] and a == f:
+      c = t
+    same_move = [f"<{f}{s}{t}>"]
+    same_move += [f"<{t}{s}{mosf[f]}>"]
+    same_move += [f"<{mosf[f]}{s}{mosf[t]}>"]
+    same_move += [f"<{mosf[t]}{s}{f}>"]
+  elif f == mosf[t]:
+    if a == f and b == mosf[c] and a is not in (b, c) and s is not in (b, c):
+      same_move = [f"<{f}{s}{t}>"]
+      same_move += [f"<{b}{s}{c}>"]
+      same_move += [f"<{t}{s}{f}>"]
+      same_move += [f"<{c}{s}{b}>"]
+      a,b, c = None
+    same_move_list.update({same_move[0]: same_move})
   print(f" same move = {same_move}")
 print(f" same move list = {same_move_list}")
 print(f" same move list length = {len(same_move_list)}")
