0b2e266055130f50129fab9757d4c0e50f14f5bd
TecxCEO <ceotecx@gmail.com>
2026-06-27 22:23:31 +0530

    Update traing_dataset_manuplator.py

--- src/model/traing_dataset_manuplator.py
+++ src/model/traing_dataset_manuplator.py
@@ -25,17 +25,17 @@ for seq_id in range(277): # 20*18*277 = 99720
         "eos_token": "<EOS>"
           }
       print(f" step = {step}")
-      step_idx += step
+      step_idx += [step]
       print(f" step_idx = {step_idx}")
     # contxt_win_bat.update({"steps" : step_idx})
     contxt_win_bat.update(steps = step_idx)
     print(f" contxt_win_bat = {contxt_win_bat}")
-    contxt_win_bates += contxt_win_bat
+    contxt_win_bates += [contxt_win_bat]
     print(f" contxt_win_bates = {contxt_win_bates}")
   # sequence.update({"context_window_batches" : contxt_win_bates})
   sequence.update(context_window_batches = contxt_win_bates)
   print(f" sequence = {sequence}")
-  sequences += sequence
+  sequences += [sequence]
   #print(f" sequences = {sequences}")
   file= "training_data_list.json"
   with open(file, "w") as f:
