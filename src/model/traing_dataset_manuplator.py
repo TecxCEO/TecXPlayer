042fe852f66e342ddfcda9ae339be28432314525
TecxCEO <ceotecx@gmail.com>
2026-06-27 21:53:13 +0530

    Update traing_dataset_manuplator.py

--- src/model/traing_dataset_manuplator.py
+++ src/model/traing_dataset_manuplator.py
@@ -20,7 +20,7 @@ for seq_id in range(277): # 20*18*277 = 99720
         "eos_token": "<EOS>"
           }
       step_idx += step
-    contxt_win_bat.update({steps : [step_idx]})
+    contxt_win_bat.update(steps : step_idx)
   contxt_win_bates += contxt_win_bat
   sequence.update({context_window_batches: cntxt_win_bates})
   sequences += sequence
