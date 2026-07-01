5e097a7c919f405b96a0fe7c21728e867b3a636b
TecxCEO <ceotecx@gmail.com>
2026-07-01 17:51:24 +0530

    Update trainingTecXcube3x3puzzlesolverModel.py

--- src/model/trainingTecXcube3x3puzzlesolverModel.py
+++ src/model/trainingTecXcube3x3puzzlesolverModel.py
@@ -12,10 +12,42 @@ import json
 import math
 import torch
 import torch.nn as nn
-from torch.nn import functional as F
-#from 
+from torch.nn import functional as F 
 import encodingdecoding as ed
-
+import cube3x3dataset20steps18perellel_batch as cdspb
+######
+state_given_to_solve={
+      "rgy":"rgy",
+      "rgw":"rgw",
+      "rby":"rby",
+      "rbw":"rbw",
+      "ogy":"ogy",
+      "ogw":"ogw",
+      "oby":"oby",
+      "obw":"obw",
+      "rb":"rb",
+      "rg":"rg",
+      "rw":"rw",
+      "ry":"ry",
+      "ob":"ob",
+      "og":"og",
+      "ow":"ow",
+      "oy":"oy",
+      "by":"by",
+      "bw":"bw",
+      "gw":"gw",
+      "gy":"gy"
+    }
+  s=cdspb.Solver()
+  ####full_response = []
+  for char in s.solve(state_given_to_solve):
+    ####sys.stdout.write(str(char) + " ")
+    #### sys.stdout.flush()
+    #### full_response += (" " + str(char)) # Collect for logging
+    ####time.sleep(7)
+    #time.sleep(0.01) 
+#### print(f" full_response = {full_response}")
+#######
 edacvr = ed.AdvancedCustomVocabularyRegistry() ###
 
 # =============================================================================
